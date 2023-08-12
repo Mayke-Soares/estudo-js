fd6159d6f2c5162926f9e29b3611ffe21c5206b2
mayke <mayke.salles.soares@gmail.com>
2023-08-12 01:47:39 -0300

    Condição Simples

---
+++ index.js
@@ -0,0 +1,7 @@
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) { // condição simples, quando se usa apenas o if.
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando o sinto de segurança!`)
