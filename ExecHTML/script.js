64586a9685e63a21338abb752f9583eeb94748b8
mayke <mayke.salles.soares@gmail.com>
2023-08-16 19:51:34 -0300

    js em html

--- ExecHTML/script.js
+++ ExecHTML/script.js
@@ -1,20 +1,22 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
-    var data = new Date
+    var data = new Date()
     var hora = data.getHours()
     var minut = data.getMinutes()
 
     msg.innerHTML = `Agora são ${hora}:${minut} horas`
-    if (hora <= 0 && hora < 12) {
+    if (hora >= 0 && hora < 12) {
         // Bom Dia!
-        
         img.src = 'manha.png'
+        document.body.style.background = '#e2cd9f'
     } else if (hora >= 12 && hora < 18) {
         // Boa Tarde!
         img.src = 'tarde.png'
+        document.body.style.background = '#b9846f'
     } else
         // Boa Noite
         img.src = 'noite.png'
+        document.body.style.background = '#515154'
 
 }
