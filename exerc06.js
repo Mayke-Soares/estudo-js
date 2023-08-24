09c847cf2cd4764b51186cf09f4b5dc9d4af7984
mayke <mayke.salles.soares@gmail.com>
2023-08-24 11:43:12 -0300

    while no começo e no final

--- exerc06.js
+++ exerc06.js
@@ -1,9 +1,19 @@
-// ESTRUTURA DE REPETIÇÃO USANDO WHILE
+// ESTRUTURA DE REPETIÇÃO USANDO WHILE com teste lógico no final.
 
 var c = 1 // Variávei c recebe o valor de 1
+
+do {  // Enquando c for menor que 6...
+    console.log('Tudo bem?') //imprima uma vez e loop...
+    c++ // Soma mais 1 até chegar no valor requerido.
+} while (c < 6)
+
+
+// ESTRUTURA DE REPETIÇÃO USANDO WHILE com teste lógico no início.
+
+/*var c = 1 // Variávei c recebe o valor de 1
 
 while (c < 6) {  // Enquando c for menor que 6...
     console.log('Tudo bem?') //imprima uma vez e loop...
     c++ // Soma mais 1 até chegar no valor requerido.
-}
+} */
 
