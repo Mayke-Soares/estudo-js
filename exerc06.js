472f6ee809d7229c9b0ca8ab881558e356557a75
mayke <mayke.salles.soares@gmail.com>
2023-08-24 11:08:44 -0300

    while

---
+++ exerc06.js
@@ -0,0 +1,9 @@
+//Estrutura de repetição usando while
+
+var c = 1 //Variávei c recebe o valor de 1
+
+while (c < 6) {  //Enquando c for menor que 6...
+    console.log('Tudo bem?') //imprima uma vez e loop...
+    c++ //Soma mais 1 até chegar no valor requerido.
+}
+
