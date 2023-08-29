af4dcadd19c35f4f0b244681da201ef606647bb8
mayke <mayke.salles.soares@gmail.com>
2023-08-29 11:56:53 -0300

    Arrey em Js

---
+++ exerc08.js
@@ -0,0 +1,14 @@
+// ESTUDO ARREY
+
+let num = [5, 8, 4]
+num[3] = 6 // Adicionando o numero 6 ao índice [3].
+
+// num.push(7) - Adicionando o número 7 ao o índice seguinte do arrey.
+/* num.length - Eles deverão ser adicionados ao Placeholder (espaço reservado).
+Para ler os e contar a quantidade de elementos de um arrey*/
+// um.sort() - Para colocar os elementos em ordem crescente
+
+console.log(num)
+console.log(`Nosso vetor é o: ${num}`)
+console.log(`O primeiro valor do vertor é ${num[0]}`)
+console.log(`O vetor tem ${num.length} elementos`)
