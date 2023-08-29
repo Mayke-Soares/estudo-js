9de5bfd110a4ca0ffc690b09b275176b4e511839
mayke <mayke.salles.soares@gmail.com>
2023-08-29 20:18:13 -0300

    for simplificado

--- exerc10.js
+++ exerc10.js
@@ -1,6 +1,11 @@
 // FOR SIMPLIFICADO
-let valores = [8, 1, 7, 4, 2, 9]
+let valores = [5, 8, 2, 9, 3]
+valores.push(1)
+valores.sort()
+console.log(valores)
 //para cada posição em num
-for(let pos in valores) {
-    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
-}
+//for(let pos in valores) {
+//console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+let pos = valores.indexOf(8)
+console.log(`O valor 8 está na posição ${pos}`)
+
